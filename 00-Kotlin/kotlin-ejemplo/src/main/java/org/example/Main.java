a7ba706d6952d61fb160bb3e14f55aeb946ab420
santiago <sanmh4@gmail.com>
2024-12-01 21:04:40 -0500

    clase 28

---
+++ 00-Kotlin/kotlin-ejemplo/src/main/java/org/example/Main.java
@@ -0,0 +1,110 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    val inmutable: String = "Santiago";
+    inmutable = "Jefferson";
+
+    val mutable: String = "Jefferson";
+    mutable = "Santiago";
+
+    val ejemploVariable = "Santiago Perea";
+    ejemploVariable.trim();
+
+    val edadEjemplo: Int = 12;
+
+    val fechaNacimiento: Date = Date()
+
+   val fechaNacimiento: Date = DAte()
+
+    when (estadoCivilWhen){
+        ("C") ->{
+            println("Casado")
+        }
+        "S0" ->{
+            println("soltero")
+        }
+        else ->{
+            println ("no sabemos")
+        }
+    }
+
+
+    val coqueteo = if (esSoltero) "Si" else "No"
+
+    fun imprimirNombre(nombre:String):: Unit{
+
+    }
+
+    fun otraFuncionAdentro(){
+        println("Otra funcion adentro")
+    }
+
+    fun imprimirNombre(nombre:String: Unit{
+        fun otraFuncionAdentro(){
+            println("otra funcion adentro")
+        }
+        println ("Nombre: ${nombre}")
+    }
+
+    println("nombre: $nombre")
+
+   println("nombre: ${nombre}")
+
+
+
+
+       class Suma(
+               unoParametro: Int,
+               dosParametro:Int,
+
+       ): Numeros(
+            unoParametro,
+            dosParametro
+    ){
+        public val SoyPublicoExplicito: String = "Publicas"
+        val soyPublicoImplicite: String = "Publico Implicito
+
+       init { //bloque de construccion primario
+        this.numeroUno
+           this.numeroDos
+           numeroUno
+           numeroDos
+           this.soyPublicoImplicite
+       soyPublicoExplicito
+
+
+
+               constructor (
+                       uno: Int?,
+                       dos: Int,
+               ):this(
+                       if(uno==null) 0 else uno,
+               dos  
+){
+            }
+       }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
